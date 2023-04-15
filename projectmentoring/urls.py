eb295f1749263b8abaeae3c06854a5e9f52ef4c0
Santiago Bolás Lago <sbl93.it@gmail.com>
2023-04-15 17:46:23 +0200

    feat: api admin and auth only on debug mode

--- projectmentoring/urls.py
+++ projectmentoring/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 from mentoring.urls import urlpatterns as mentoring_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     # mentoring app url patterns
     path('mentoring/', include(mentoring_urls))
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path('admin/', admin.site.urls))
+    urlpatterns.append(path('api-auth/', include('rest_framework.urls')))
