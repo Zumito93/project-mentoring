ef4a0f9eabe8913ec51a1c1f638e3f4cb279f98b
Santiago Bolás Lago <sbl93.it@gmail.com>
2023-04-17 11:25:00 +0200

    fix: id urls no trailing /

--- mentoring/urls.py
+++ mentoring/urls.py
@@ -5,11 +5,11 @@ from mentoring import views
 urlpatterns = [
     # Mentor model views
     path('mentor', views.MentorListView.as_view(), name='MentorListView'),
-    path('mentor/<int:pk>/', views.MentorDetailView.as_view(), name='MentorDetailView'),
+    path('mentor/<int:pk>', views.MentorDetailView.as_view(), name='MentorDetailView'),
     # Project model views
     path('project', views.ProjectListView.as_view(), name='ProjectListView'),
-    path('project/<int:pk>/', views.ProjectDetailView.as_view(), name='ProjectDetailView'),
+    path('project/<int:pk>', views.ProjectDetailView.as_view(), name='ProjectDetailView'),
     # Mentorship model views
     path('mentorship', views.MentorshipListView.as_view(), name='MentorshipListView'),
-    path('mentorship/<int:pk>/', views.MentorshipDetailView.as_view(), name='MentorshipDetailView'),
+    path('mentorship/<int:pk>', views.MentorshipDetailView.as_view(), name='MentorshipDetailView'),
 ]
