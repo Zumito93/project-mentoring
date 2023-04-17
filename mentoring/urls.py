bdca202581063a7035dd14faf0de214e3133f9a6
Santiago Bolás Lago <sbl93.it@gmail.com>
2023-04-17 11:26:00 +0200

    fix: plural url nouns

--- mentoring/urls.py
+++ mentoring/urls.py
@@ -4,12 +4,12 @@ from mentoring import views
 
 urlpatterns = [
     # Mentor model views
-    path('mentor', views.MentorListView.as_view(), name='MentorListView'),
-    path('mentor/<int:pk>', views.MentorDetailView.as_view(), name='MentorDetailView'),
+    path('mentors', views.MentorListView.as_view(), name='MentorListView'),
+    path('mentors/<int:pk>', views.MentorDetailView.as_view(), name='MentorDetailView'),
     # Project model views
-    path('project', views.ProjectListView.as_view(), name='ProjectListView'),
-    path('project/<int:pk>', views.ProjectDetailView.as_view(), name='ProjectDetailView'),
+    path('projects', views.ProjectListView.as_view(), name='ProjectListView'),
+    path('projects/<int:pk>', views.ProjectDetailView.as_view(), name='ProjectDetailView'),
     # Mentorship model views
-    path('mentorship', views.MentorshipListView.as_view(), name='MentorshipListView'),
-    path('mentorship/<int:pk>', views.MentorshipDetailView.as_view(), name='MentorshipDetailView'),
+    path('mentorships', views.MentorshipListView.as_view(), name='MentorshipListView'),
+    path('mentorships/<int:pk>', views.MentorshipDetailView.as_view(), name='MentorshipDetailView'),
 ]
